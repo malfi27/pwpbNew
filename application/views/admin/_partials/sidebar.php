94447df17441bac79e7e30cc1be0f7faa2d5afc3
unknown <ramdhaniaza09@gmail.com>
2021-11-11 11:18:59 +0700

    last update for now 11 nov 2021

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -16,7 +16,7 @@
                 
                 <li
                     class="sidebar-item active ">
-                    <a href="index.html" class='sidebar-link'>
+                    <a href="<?php echo base_url('/index.php/admin');?>" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                     </a>
